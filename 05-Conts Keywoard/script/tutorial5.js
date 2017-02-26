8973549b0cd8487790be99cd3d5b59d927511847
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-02-26 17:51:21 +0700

    const

---
+++ 05-Conts Keywoard/script/tutorial5.js
@@ -0,0 +1,12 @@
+var a;
+var b = 10;
+var obj1 = {
+    'nama': 'Muhammad Andika Nugraha'
+};
+console.log(obj1.nama);
+var obj2 = {
+    'nama': 'Dika'
+};
+obj1.nama = "dika";
+console.log(obj1.nama);
+//# sourceMappingURL=tutorial5.js.map
---
+++ 05-Conts Keywoard/tutorial5.ts
@@ -0,0 +1,16 @@
+let a;
+const b=10;
+
+const obj1 = {
+	'nama' : 'Muhammad Andika Nugraha'
+}
+
+console.log(obj1.nama);
+
+var obj2 = {
+	'nama' : 'Dika'
+}
+
+obj1.nama = "dika";
+
+console.log(obj1.nama);
