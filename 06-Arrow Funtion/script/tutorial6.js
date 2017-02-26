28814f7b5061b737199b0bbb5f16e11a674ca185
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-02-26 18:13:37 +0700

    Arrow function

---
+++ 06-Arrow Funtion/script/tutorial6.js
@@ -0,0 +1,9 @@
+var angka = function () { return 10; };
+var angka = function () {
+    return 10;
+};
+var angka1 = function () { return 10; };
+var angka2 = function (e) { return 10 + e; };
+console.log(angka2(5));
+(function () { return 3; });
+//# sourceMappingURL=tutorial6.js.map
---
+++ 06-Arrow Funtion/tutorial6.ts
@@ -0,0 +1,11 @@
+var angka = () => { return 10}
+var angka = function(){
+	return 10;
+}
+var angka1 = () =>10;
+
+var angka2 = e => 10+e;
+
+console.log(angka2(5));
+
+()=>3; // function(){return 3}
