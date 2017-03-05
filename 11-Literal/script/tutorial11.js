b4ba9ce39a20f26d5a80cb6d4d4c75b438c00a1d
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-03-05 21:45:50 +0700

    Object literal

--- 11-Literal/script/tutorial11.js
+++ 11-Literal/script/tutorial11.js
@@ -1,2 +1,29 @@
-tutorial11.ts;
+var firstName = "Andika";
+var lastName = "Nugraha";
+var person = {
+    firstName: firstName,
+    lastName: lastName
+};
+//--------------------
+function createPerson(firstName, lastName, age) {
+    var fullName = firstName + " " + lastName;
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        fullName: fullName,
+        isSenior: function () {
+            return age > 30;
+        }
+    };
+}
+//-------------------------
+console.log(person);
+console.log(person.firstName);
+console.log(person.lastName);
+var test = createPerson("Muhammad", "Andika", 31);
+console.log(test);
+console.log(test.firstName);
+console.log(test.lastName);
+console.log(test.fullName);
+console.log(test.isSenior());
 //# sourceMappingURL=tutorial11.js.map
--- 11-Literal/tutorial11.ts
+++ 11-Literal/tutorial11.ts
@@ -1,1 +1,33 @@
-tutorial11.ts
+let firstName	= "Andika";
+let lastName	= "Nugraha";
+
+let person = {
+	firstName 	,
+	lastName	: lastName
+}
+
+//--------------------
+
+function createPerson(firstName,lastName,age){
+	let fullName = firstName + " " + lastName;
+	return {
+				firstName,
+				lastName,
+				fullName,
+				isSenior(){
+					return age>30;
+				}
+			}
+}
+
+//-------------------------
+console.log(person);
+console.log(person.firstName);
+console.log(person.lastName);
+
+let test = createPerson("Muhammad","Andika",31);
+console.log(test);
+console.log(test.firstName);
+console.log(test.lastName);
+console.log(test.fullName);
+console.log(test.isSenior());
