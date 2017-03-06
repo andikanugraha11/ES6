2a63acab2aa9374f5cfa4a07e8088e4c7dbb380b
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-03-06 14:12:34 +0700

    String Template

---
+++ 14-String Template/script/tutorial14.js
@@ -0,0 +1,8 @@
+var user = "AndikaNugraha";
+//javascript
+var greeting = "Selamat datang " + user + " di Applikasi kami";
+//ES6 (whitespace dihitung)
+var greetinges6 = "Selamat \"datang\" " + user + " di 'aplikasi'\t kami";
+console.log(greeting);
+console.log(greetinges6);
+//# sourceMappingURL=tutorial14.js.map
---
+++ 14-String Template/tutorial14.ts
@@ -0,0 +1,10 @@
+let user = "AndikaNugraha";
+
+//javascript
+let greeting = "Selamat datang " + user + " di Applikasi kami";
+
+//ES6 (whitespace dihitung)
+let greetinges6 = `Selamat "datang" ${user} di 'aplikasi'	 kami`;
+
+console.log(greeting);
+console.log(greetinges6);
