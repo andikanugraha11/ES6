465da4ce332bb6ba1d6b879aa99ddac35d84c0d3
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-03-06 14:37:17 +0700

    For of Loop

---
+++ 15-For of Loop/script/tutorial15.js
@@ -0,0 +1,14 @@
+var warna = ['merah', 'kuning', 'hijau'];
+for (var index in warna) {
+    console.log(warna[index]);
+}
+for (var _i = 0, warna_1 = warna; _i < warna_1.length; _i++) {
+    var warn = warna_1[_i];
+    console.log(warn);
+}
+var string = "ANDIKA";
+for (var _a = 0, string_1 = string; _a < string_1.length; _a++) {
+    var letter = string_1[_a];
+    console.log(letter);
+}
+//# sourceMappingURL=tutorial15.js.map
---
+++ 15-For of Loop/tutorial15.ts
@@ -0,0 +1,15 @@
+let warna = ['merah','kuning','hijau'];
+
+for(let index in warna){
+	console.log(warna[index]);
+}
+
+for(let warn of warna){
+	console.log(warn);
+}
+
+let string = "ANDIKA";
+
+for(let letter of string){
+	console.log(letter);
+}
