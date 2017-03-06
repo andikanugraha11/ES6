6ad9dd40f95291ebd787e706fe2678e62527b927
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-03-06 13:52:20 +0700

    desctruct object

---
+++ 13-Destruct Object/script/tutorial13.js
@@ -0,0 +1,8 @@
+var mahasiswa = {
+    firstName: "Andika",
+    lastName: "Nugraha",
+    age: 21
+};
+var f = mahasiswa.firstName, l = mahasiswa.lastName, a = mahasiswa.age;
+console.log(f);
+//# sourceMappingURL=tutorial13.js.map
---
+++ 13-Destruct Object/tutorial13.ts
@@ -0,0 +1,8 @@
+let mahasiswa = {
+	firstName	: "Andika",
+	lastName	: "Nugraha",
+	age			: 21
+}
+
+let {firstName : f, lastName : l, age : a} = mahasiswa;
+console.log(f);
